3b9d960133b778b22d396a3e4d4cce9f7cc22db0
Mara Priscilla Barros Souza <priscillabarrossouza@gmail.com>
2021-08-13 16:14:10 -0300

    projeto de sexta

---
+++ variaveis/index.js
@@ -0,0 +1,64 @@
+// vai imprimir 10,5
+
+//let a = 10
+//let b = 5
+//console.log(a,b)
+
+//let horasDeTrabalho = prompt("quantas horas vc trabalha?")
+//let salario = prompt("qual o seu salario")
+//console.log(`ola,vc trabalha ${horasDeTrabalho} horas por dia e seu salario eh ${salario}.00 por mes`)
+
+//Declare uma variável para armazenar um nome, sem atribuir um valor.
+//Declare uma variável para armazenar uma idade, sem atribuir um valor
+//Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando 
+
+//const nome = "mara"
+//const idade = 35
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+/*Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores 
+às variáveis que acabou de criar */
+
+//const nome = prompt("Qual seu nome?")
+//const idade = prompt("qual sua idade?")
+//console.log("Ola, seu nome eh", nome, "e sua idade é", idade, "anos")
+
+//const perguntaFeliz = prompt("Voce esta feliz?")
+//const cidade = prompt("vc mora em sao paulo?")
+//const crianca = prompt("voce eh uma crianca")
+//console.log(`ola, hoje vc esta feliz? ${perguntaFeliz} vc mora em sao paulo? ${cidade} vc eh crianca ${crianca}`)
+
+
+ //let a = 10
+ //let b = 34
+ //console.log(a, b)
+
+ //a = 13
+ //b = 30
+ //console.log(a, b)
+
+ //let cor = "azul"
+ //let cor1 = "branco"
+ //console.log(cor, cor1)
+
+ //cor = "rosa"
+ //cor1 = "amarelo"
+ //console.log(cor, cor1)
+
+ //let copo = "suco de maracuja"
+ //let copo1 = "coca cola"
+ //console.log(copo, copo1)
+
+ //copo = "suco de uva"
+//copo1 = "agua"
+//console.log(copo,copo1)
+
+const numero = prompt("preciso de um numero")
+const numero1 = prompt("preciso de outro numero")
+
+const numero2 = numero / numero1
+console.log(numero2)
+
+
+
