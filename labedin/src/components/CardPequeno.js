1c844e60b1621be7e8fe2b6849d98a5bc765c451
Mara Priscilla Barros Souza <priscillabarrossouza@gmail.com>
2021-09-08 16:20:30 -0300

    exercico react

---
+++ labedin/src/components/CardPequeno.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+      <div className="App">
+        <div className="page-section-container">
+          <h2>Dados pessoais</h2>
+          <CardPequeno
+                Email="marapriscilla@uol.com" 
+          />
+               </div>
+    </div>
+  );
+}
+
+export default App;
